182a76d9feaa4ebaa05aac41dc8f1c8fde848e47
ycloudo <cces994512@gmail.com>
2021-11-04 13:20:41 +0800

    v2

--- project.js
+++ project.js
@@ -1,22 +1,31 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var map = {
-    x: 160,
-    y: 80
+    x: 120,
+    y: 60
 }
-var speed = 50;
+var speed = 100;
 canvas.width = map.x * 8;
 canvas.height = map.y * 8;
+
 var cells = new Array();
-for (var i = 0; i < map.x; i++) {
+for (let i = 0; i < map.y; i++) {
     cells[i] = new Array();
-    for (var j = 0; j < map.y; j++) {
+    for (let j = 0; j < map.x; j++) {
         cells[i][j] = "";
     }
 }
+
+var next = new Array();
+for (let n = 0; n < map.y; n++) {
+    next[n] = new Array();
+    for (let p = 0; p < map.x; p++) {
+        next[n][p] = "";
+    }
+}
 //填充初始顏色
-for (var i = 0; i < map.x; i++) {
-    for (var j = 0; j < map.y; j++) {
+for (let i = 0; i < map.y; i++) {
+    for (let j = 0; j < map.x; j++) {
         cells[i][j] = "white";
     }
 }
@@ -24,29 +33,32 @@ for (var i = 0; i < map.x; i++) {
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     var new_cells = cells.flat();
-    for (var i = 0; i < new_cells.length; i++) {
-        color = new_cells[i];
+    for (let i = 0; i < new_cells.length; i++) {
+        var color = new_cells[i];
         context.fillStyle = color;
-        context.fillRect((i % map.x) * 8, i / map.x * 8, 7, 7);
+        context.fillRect((i % map.x) * 8, parseInt(i / map.x) * 8, 7, 7);
     }
     life();
 }
 //設定初始細胞
 function seed() {
     var seeds = 800;
-    for (var i = 0; i < seeds; i++) {
-        cells[parseInt(Math.random() * map.x)][parseInt(Math.random() * map.y)] = "black";
+    for (let i = 0; i < seeds; i++) {
+        cells[parseInt(Math.random() * map.y)][parseInt(Math.random() * map.x)] = "black";
     }
 }
 //生命模式
 //存活:黑色
 //死亡:白色
 function life() {
-    for (var i = 0; i < map.x; i++) {
-        for (var j = 0; j < map.y; j++) {
+    var count = 0//統計周圍有幾個細胞存活
+
+    for (let i = 0; i < map.y; i++) {
+        for (let j = 0; j < map.x; j++) {
+            count = 0;
             var temp = cells[i][j];
+            // console.log(temp);
             if (temp == "black") {//如果此細胞狀態為存活
-                var count = 0;//統計周圍有幾個細胞存活
                 count += check_cell(i - 1, j - 1);//左上
                 count += check_cell(i - 1, j);//中上
                 count += check_cell(i - 1, j + 1);//右上
@@ -56,15 +68,14 @@ function life() {
                 count += check_cell(i + 1, j);//中下
                 count += check_cell(i + 1, j + 1);//右下
                 // console.log(count);
-                if (count < 2) {
-                    cells[i][j] = "white";
-                } else if (count == 2 || count == 3) {
-                    cells[i][j] = cells[i][j];
-                } else if (count > 3) {
-                    cells[i][j] = "white";
+                if (count < 2 || count > 3) {
+                    next[i][j] = "white";
+                } else {
+                    next[i][j] = "black";
                 }
+                // console.log(next[i][j]);
+
             } else {//細胞為死亡狀態
-                var count = 0;//統計周圍有幾個細胞存活
                 count += check_cell(i - 1, j - 1);//左上
                 count += check_cell(i - 1, j);//中上
                 count += check_cell(i - 1, j + 1);//右上
@@ -73,27 +84,36 @@ function life() {
                 count += check_cell(i + 1, j - 1);//左下
                 count += check_cell(i + 1, j);//中下
                 count += check_cell(i + 1, j + 1);//右下
+                // console.log(count);
                 if (count == 3) {
-                    cells[i][j] = "black";
+                    next[i][j] = "black";
+                } else {
+                    next[i][j] = "white";
                 }
+                // console.log(next[i][j]);
             }
         }
     }
+    for (let l = 0; l < map.y; l++) {
+        for (let m = 0; m < map.x; m++) {
+            cells[l][m] = next[l][m];
+        }
+    }
 }
-function check_cell(i, j) {//檢查cells有無生命
-    if (i < 0 || j < 0) {
+function check_cell(a, b) {//檢查cells有無生命
+    if (a < 0 || b < 0) {
         return 0;
-    } else if (i > cells.length) {
+    } else if (a > map.y - 1) {
         return 0;
-    } else if (j > cells[j].length) {
+    } else if (b > map.x - 1) {
         return 0;
-    } else if (cells[i][j] == "black") {
+    } else if (cells[a][b] == "black") {
         return 1;
     } else {
         return 0;
     }
-}
 
+}
 seed();
 setInterval(draw, speed);
 

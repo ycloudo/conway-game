61bf263bb81ec0e66019abcfcd61bff9e05c7f95
ycloudo <cces994512@gmail.com>
2021-11-06 00:57:20 +0800

    v2

--- project.js
+++ project.js
@@ -1,32 +1,46 @@
+$(document).ready(function () {
+    $("#start").click(function () {
+        document.getElementById("seeds").disabled = true;
+        document.getElementById("start").disabled = true;
+    });
+    $("#pass").click(function () {
+        document.getElementById("start").disabled = false;
+    });
+    $("#restart").click(function () {
+        document.getElementById("start").disabled = false;
+        document.getElementById("seeds").disabled = false;
+    });
+});
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var map = {
-    x: 120,
-    y: 60
+    x: 230,
+    y: 144
 }
 var speed = 100;
-canvas.width = map.x * 8;
-canvas.height = map.y * 8;
-
+var size = 5;
+canvas.width = map.x * size;
+canvas.height = map.y * size;
 var cells = new Array();
-for (let i = 0; i < map.y; i++) {
-    cells[i] = new Array();
-    for (let j = 0; j < map.x; j++) {
-        cells[i][j] = "";
-    }
-}
-
 var next = new Array();
-for (let n = 0; n < map.y; n++) {
-    next[n] = new Array();
-    for (let p = 0; p < map.x; p++) {
-        next[n][p] = "";
+//建立初始陣列
+function construct() {
+    for (let i = 0; i < map.y; i++) {
+        cells[i] = new Array();
+        for (let j = 0; j < map.x; j++) {
+            cells[i][j] = "";
+        }
+    } for (let n = 0; n < map.y; n++) {
+        next[n] = new Array();
+        for (let p = 0; p < map.x; p++) {
+            next[n][p] = "";
+        }
     }
-}
-//填充初始顏色
-for (let i = 0; i < map.y; i++) {
-    for (let j = 0; j < map.x; j++) {
-        cells[i][j] = "white";
+    //填充初始顏色
+    for (let i = 0; i < map.y; i++) {
+        for (let j = 0; j < map.x; j++) {
+            cells[i][j] = "white";
+        }
     }
 }
 //畫出方格
@@ -36,13 +50,22 @@ function draw() {
     for (let i = 0; i < new_cells.length; i++) {
         var color = new_cells[i];
         context.fillStyle = color;
-        context.fillRect((i % map.x) * 8, parseInt(i / map.x) * 8, 7, 7);
+        context.fillRect((i % map.x) * size, parseInt(i / map.x) * size, size - 1, size - 1);
     }
+}
+//onload
+function onload() {
+    construct();
+    seed();
+    draw();
+}
+function play() {
+    draw();
     life();
 }
 //設定初始細胞
 function seed() {
-    var seeds = 800;
+    var seeds = 8000;
     for (let i = 0; i < seeds; i++) {
         cells[parseInt(Math.random() * map.y)][parseInt(Math.random() * map.x)] = "black";
     }
@@ -52,7 +75,6 @@ function seed() {
 //死亡:白色
 function life() {
     var count = 0//統計周圍有幾個細胞存活
-
     for (let i = 0; i < map.y; i++) {
         for (let j = 0; j < map.x; j++) {
             count = 0;
@@ -112,8 +134,72 @@ function check_cell(a, b) {//檢查cells有無生命
     } else {
         return 0;
     }
-
 }
-seed();
-setInterval(draw, speed);
+//開始
+function start() {
+    p = setInterval(play, speed);
+}
+//重新開始
+function restart() {
+    pass();
+    construct();
+    draw();
+}
+//暫停
+function pass() {
+    clearInterval(p);
+}
+//產生隨機細胞
+function random_seed() {
+    construct();
+    seed();
+    draw();
+}
+function seed_gun() {
+    construct();
+    cells[5][1] = "black";
+    cells[5][2] = "black";
+    cells[6][1] = "black";
+    cells[6][2] = "black";
+    cells[5][11] = "black";
+    cells[6][11] = "black";
+    cells[7][11] = "black";
+    cells[4][12] = "black";
+    cells[8][12] = "black";
+    cells[3][13] = "black";
+    cells[9][13] = "black";
+    cells[3][14] = "black";
+    cells[9][14] = "black";
+    cells[6][15] = "black";
+    cells[4][16] = "black";
+    cells[8][16] = "black";
+    cells[5][17] = "black";
+    cells[6][17] = "black";
+    cells[7][17] = "black";
+    cells[6][18] = "black";
+    cells[3][21] = "black";
+    cells[4][21] = "black";
+    cells[5][21] = "black";
+    cells[3][22] = "black";
+    cells[4][22] = "black";
+    cells[5][22] = "black";
+    cells[2][23] = "black";
+    cells[6][23] = "black";
+    cells[1][25] = "black";
+    cells[2][25] = "black";
+    cells[6][25] = "black";
+    cells[7][25] = "black";
+    cells[3][35] = "black";
+    cells[4][35] = "black";
+    cells[3][36] = "black";
+    cells[4][36] = "black";
+    draw();
+}
+function selectSeed(evt) {
+    if (evt.target.value === "random") {
+        random_seed();
+    } else if (evt.target.value === "gun") {
+        seed_gun();
+    }
+}
 
